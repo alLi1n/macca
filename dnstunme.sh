c2296343c4950251e26c4dbf0efd6b80d09342f1
alLi1n <ex_noer@ymail.com>
2014-05-15 23:26:36 +0800

    Update dnstunme.sh

--- dnstunme.sh
+++ dnstunme.sh
@@ -2,4 +2,4 @@
 wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 rpm -Uvh epel-release-6-8.noarch.rpm
 yum install -y iodine
-iodined -f 10.1.5.1 -P 1234gosok dnstunme.mooo.com -DD
+iodined -c  10.1.5.1 -P 1234gosok dnstunme.mooo.com 
